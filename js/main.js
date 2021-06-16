5e07bfe4206e4871bfb903e45b7a352eafc7187e
Мария <sophie1235689@gmail.com>
2021-06-16 21:46:57 +0300

    2_functions

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-
 function returnRandomInteger(min, max){
+  if (min < 0) {
+    return 'min не может быть меньше 0';
+  }
   min = Math.abs(min);
   max = Math.abs(max);
   const randomInt = min + Math.random() * (max - min);
 
   if (min < max) {
     return Math.round(randomInt);
-  } else if (min === max){
-    return 'твой диапазон вовсе не диапазон :))';
-  } else {
-    return 0;
+  } else if ((min === max) || (min > max)){
+    return 'max должен быть больше, чем min';
   }
 }
 
 returnRandomInteger(3, 10);
 
-
 function returnRandomFloat(min, max){
+  if (min < 0) {
+    return 'min не может быть меньше 0';
+  }
   min = Math.abs(min);
   max = Math.abs(max);
   const randomFloat = min + Math.random() * (max - min);
@@ -25,10 +27,7 @@ function returnRandomFloat(min, max){
     return randomFloat;
   } else if (min === max){
     return 'твой диапазон вовсе не диапазон, стоит ли поправить ситуацию';
-  } else {
-    return 0;
   }
 }
 
-
-returnRandomFloat(3, 10));
+returnRandomFloat(3, 10);
