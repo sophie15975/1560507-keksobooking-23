099d107f60f1a7cfcec86e6e380dbbce729b9dec
Мария <sophie1235689@gmail.com>
2021-07-19 22:14:44 +0300

    fixes

--- js/generateAdPosition.js
+++ js/generateAdPosition.js
@@ -1,5 +1,6 @@
 import {createAdvertismentsArray} from './createAdvertismentsArray.js';
 import {makeElement}  from './makeElement.js';
+import {hideIftextContentEmpty} from './hideIftextContentEmpty.js';
 
 const advertisments = createAdvertismentsArray();
 
@@ -16,96 +17,74 @@ export function generateAdPosition(){
     const title = makeElement('h3', 'popup__title');
     title.textContent = advertisments[index].offer.title;
     article.appendChild(title);
-    if(title.textContent === null){
-      title.style.display ='none';
-    }
+    hideIftextContentEmpty(title);
 
     const address = makeElement('div', 'popup__text--address');
     address.textContent = advertisments[index].offer.address;
     article.appendChild(address);
-    if(address.textContent === null){
-      address.style.display ='none';
-    }
+    hideIftextContentEmpty(address);
 
     const price = makeElement('div','popup__text--price');
     price.textContent = `${advertisments[index].offer.price} ₽/ночь`;
     article.appendChild(price);
-    if(price.textContent === null){
-      price.style.display ='none';
-    }
+    hideIftextContentEmpty(price);
 
     const typeElem =  makeElement('h4', 'popup__type');
     const type =  advertisments[index].offer.type;
-    if(type === 'flat'){
-      typeElem.textContent = 'Квартира';
+
+    switch (type) {
+      case 'flat':
+        typeElem.textContent = 'Квартира';
+        break;
+      case 'bungalow':
+        typeElem.textContent = 'Бунгало';
+        break;
+      case  'house':
+        typeElem.textContent = 'Дом';
+        break;
+      case  'palace':
+        typeElem.textContent = 'Дворец';
+        break;
+      case 'hotel':
+        typeElem.textContent = 'Отель';
+        break;
+
     }
-    if(type === 'bungalow'){
-      typeElem.textContent = 'Бунгало';
-    }
-    if(type === 'house'){
-      typeElem.textContent = 'Дом';
-    }
-    if(type === 'palace'){
-      typeElem.textContent = 'Дворец';
-    }
-    if(type === 'hotel'){
-      typeElem.textContent = 'Отель';
-    }
+
     article.appendChild(typeElem);
-    if(typeElem.textContent === null){
-      typeElem.style.display ='none';
-    }
+    hideIftextContentEmpty(typeElem);
 
     const roomInfo = makeElement('p','popup__text--capacity');
     roomInfo.textContent = `${advertisments[index].offer.rooms} комнаты для ${advertisments[index].offer.guests} гостей`;
     article.appendChild(roomInfo);
-    if(roomInfo.textContent === null){
-      roomInfo.style.display ='none';
-    }
+    hideIftextContentEmpty(roomInfo);
 
     const timeElem = makeElement('p', 'popup__text--time');
     const time = `Заезд после ${advertisments[index].offer.checkin}, выезд до ${advertisments[index].offer.checkout}`;
     timeElem.textContent = time;
     article.appendChild(timeElem);
-    if(timeElem.textContent === null){
-      timeElem.style.display ='none';
-    }
+    hideIftextContentEmpty(timeElem);
 
     const featureElem = makeElement('ul', 'popup__features');
     const features = advertisments[index].offer.features;
-    for (let iks = 0; iks < features.length; iks++){
+    for (let iterator = 0; iterator < features.length; iterator++){
       const liElem = makeElement('li', 'popup__feature');
       featureElem.appendChild(liElem);
-      liElem.textContent = features[iks];
+      liElem.textContent = features[iterator];
     }
     article.appendChild(featureElem);
-    if(featureElem.textContent === null){
-      featureElem.style.display ='none';
-    }
+    hideIftextContentEmpty(featureElem);
 
     const description = makeElement('p', 'popup__description');
     description.textContent = advertisments[index].offer.description;
-    console.log(description.textContent);
-    if(description.textContent === null){
-      description.style.display ='none';
-    }
+    hideIftextContentEmpty(description);
     article.appendChild(description);
-
-
-    /*const description = makeElement('p', 'popup__description');
-    if (advertisments[index].offer.description){
-      description.textContent = advertisments[index].offer.description;
-      article.appendChild(description);
-    }
-    else{
-      description.style.display ='none';
-    } */
 
     const photosElem = makeElement('div', 'popup__photos');
     const photos = advertisments[index].offer.photos;
-    for (let igrik = 0; igrik < photos.length; igrik++){
+    for (let iterator = 0; iterator < photos.length; iterator++){
       const image = makeElement('img', 'popup__photo');
-      image.src = photos[igrik];
+      image.src = photos[iterator];
       photosElem.appendChild(image);
     }
     article.appendChild(photosElem);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {generateAdPosition} from './generateAdPosition.js';
 
 const adPositionArray = generateAdPosition();
-console.log(adPositionArray);
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(adPositionArray[0]);
